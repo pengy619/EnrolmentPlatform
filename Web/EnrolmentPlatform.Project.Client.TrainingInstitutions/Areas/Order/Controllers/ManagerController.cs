6882d1e80b8c36ce310ce77bbde7fc62a7cd0317
chaichang <chaichang@CHAICHANG-PC>
2018-12-01 13:56:54 +0800

    1

---
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/ManagerController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers;
+
+namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Controllers
+{
+    public class ManagerController : BaseController
+    {
+        // GET: Order/Order
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Payment/Controllers/PaymentController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers;
+
+namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Payment.Controllers
+{
+    public class PaymentController : BaseController
+    {
+        // GET: Payment/Payment
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
