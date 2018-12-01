d4a9af0b3be1141b696546d71b205e6e6622dca6
chaichang <chaichang@CHAICHANG-PC>
2018-12-01 14:26:50 +0800

    1

--- Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Payment/Controllers/ManagerController.cs
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Payment/Controllers/ManagerController.cs
@@ -7,9 +7,9 @@ using EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers;
 
 namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Payment.Controllers
 {
-    public class PaymentController : BaseController
+    public class ManagerController : BaseController
     {
-        // GET: Payment/Payment
+        // GET: Payment/Manager
         public ActionResult Index()
         {
             return View();
---
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Payment/PaymentAreaRegistration.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Payment
+{
+    public class PaymentAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Payment";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Payment_default",
+                "Payment/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional },
+                namespaces: new string[] { "EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Payment.Controllers" }
+            );
+        }
+    }
+}
