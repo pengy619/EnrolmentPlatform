7004ab4d6a098f82892740733550dd745f1dafcc
彭阳 <pengy@tripscm.com>
2018-12-02 13:55:51 +0800

    完成用户管理

---
+++ Web/EnrolmentPlatform.Project.Client.Admin/Areas/Account/Controllers/LearningCenterController.cs
@@ -0,0 +1,78 @@
+﻿using EnrolmentPlatform.Project.Client.Admin.Controllers;
+using EnrolmentPlatform.Project.DTO.Accounts;
+using EnrolmentPlatform.Project.DTO.Enums.Systems;
+using EnrolmentPlatform.Project.Infrastructure;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnrolmentPlatform.Project.Client.Admin.Areas.Account.Controllers
+{
+    public class LearningCenterController : BaseController
+    {
+        /// <summary>
+        /// 学习中心管理
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 学习中心操作
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult Option(Guid? supplierId)
+        {
+            EnterpriseAddDto dto = new EnterpriseAddDto();
+            if (supplierId.HasValue)
+            {
+                Dictionary<string, string> param = new Dictionary<string, string>();
+                param.Add("enterpriseId", supplierId.Value.ToString());
+                Tuple<string, string> parameters = WebApiHelper.GetQueryString(param);
+                var ret = WebApiHelper.Get<HttpResponseMsg>(
+                    "/api/Enterprise/GetEnterpriseById", parameters.Item1, parameters.Item2,
+                    ConfigurationManager.AppSettings["StaffId"].ToInt());
+                if (ret.IsSuccess)
+                {
+                    dto = ret.Data.ToString().ToObject<EnterpriseAddDto>();
+                }
+            }
+            return View(dto);
+        }
+
+        /// <summary>
+        /// 保存学习中心
+        /// </summary>
+        /// <param name="dto"></param>
+        /// <returns></returns>
+        [HttpPost]
+        [ValidateInput(false)]
+        public async Task<ActionResult> SaveSupplier(EnterpriseAddDto dto)
+        {
+            dto.Classify = SystemTypeEnum.LearningCenter;
+            dto.CurUserId = this.UserId;
+            dto.CurUserAccount = this.UserAccount;
+            if (dto.EnterpriseId.IsEmpty())
+            {
+                var ret = await WebApiHelper.PostAsync<HttpResponseMsg>("/api/Enterprise/AddEnterprise",
+                    JsonConvert.SerializeObject(dto),
+                    ConfigurationManager.AppSettings["StaffId"].ToInt());
+                return Json(ret);
+            }
+            else
+            {
+                var ret = await WebApiHelper.PostAsync<HttpResponseMsg>("/api/Enterprise/UpdateEnterpriseInfo",
+                    JsonConvert.SerializeObject(dto),
+                    ConfigurationManager.AppSettings["StaffId"].ToInt());
+                return Json(ret);
+            }
+        }
+    }
+}
