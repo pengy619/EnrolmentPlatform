083fde0b779543a66b13407292ecde06d4ae236e
chaichang <chaichang@CHAICHANG-PC>
2018-12-02 18:18:05 +0800

    123

--- Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/ImageController.cs
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/ImageController.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers;
 
 namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Controllers
 {
-    public class ImageController : Controller
+    public class ImageController : BaseController
     {
         // GET: Order/Image
         public ActionResult Index()
