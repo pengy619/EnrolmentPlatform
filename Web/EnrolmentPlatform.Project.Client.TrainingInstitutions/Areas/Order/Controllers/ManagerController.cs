d72590f81a9e008fc1bddb972cae973a0f64ddb4
chaichang <chaichang@CHAICHANG-PC>
2018-12-02 17:56:34 +0800

    导出完成！

--- Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/ManagerController.cs
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/ManagerController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,6 +8,8 @@ using EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers;
 using EnrolmentPlatform.Project.DTO;
 using EnrolmentPlatform.Project.DTO.Orders;
 using EnrolmentPlatform.Project.Infrastructure;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
 
 namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Controllers
 {
@@ -19,6 +22,81 @@ namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Cont
         public ActionResult Index()
         {
             return View();
+        }
+
+        /// <summary>
+        /// 导出
+        /// </summary>
+        /// <param name="param">param</param>
+        /// <returns></returns>
+        public ActionResult Export(OrderListReqDto param)
+        {
+            int reCount = 0;
+            List<OrderListDto> list = OrderService.GetStudentList(param, ref reCount);
+            if (list == null || list.Count == 0)
+            {
+                return Content("没有任何可以导出的数据！");
+            }
+
+            #region 导出
+
+            HSSFWorkbook hssfworkbook = null;
+            try
+            {
+                using (FileStream file = new FileStream(this.Server.MapPath("~/Temp/OrderTemp.xls"), FileMode.Open, FileAccess.Read))
+                {
+                    hssfworkbook = new HSSFWorkbook(file);
+                }
+            }
+            catch (Exception ex)
+            {
+            }
+
+            HSSFSheet sheet = (HSSFSheet)hssfworkbook.GetSheet("data");
+
+            int startRow = 2;
+            var tempDataRow = list.First();
+            HSSFRow tempRow = (HSSFRow)sheet.GetRow(startRow);
+            tempRow.Cells[0].SetCellValue(tempDataRow.StudentName);
+            tempRow.Cells[1].SetCellValue(tempDataRow.BatchName);
+            tempRow.Cells[2].SetCellValue(tempDataRow.SchoolName);
+            tempRow.Cells[3].SetCellValue(tempDataRow.LevelName);
+            tempRow.Cells[4].SetCellValue(tempDataRow.MajorName);
+            tempRow.Cells[5].SetCellValue(tempDataRow.StatusName);
+            tempRow.Cells[6].SetCellValue(tempDataRow.CreateTimeStr);
+            tempRow.Cells[7].SetCellValue(tempDataRow.CreateUserName);
+
+            for (int a = 1; a < list.Count; a++)
+            {
+                HSSFRow row = (HSSFRow)sheet.CreateRow(startRow + a);
+                row.HeightInPoints = tempRow.HeightInPoints;
+                row.Height = tempRow.Height;
+
+                //创建列
+                for (int c = 0; c < tempRow.Cells.Count; c++)
+                {
+                    ICell cell = row.CreateCell(c);
+                    ICell sourceCell = tempRow.GetCell(c);
+                    cell.CellStyle = sourceCell.CellStyle;
+                    cell.SetCellType(sourceCell.CellType);
+                }
+                var dto = list[a];
+                row.Cells[0].SetCellValue(dto.StudentName);
+                row.Cells[1].SetCellValue(dto.BatchName);
+                row.Cells[2].SetCellValue(dto.SchoolName);
+                row.Cells[3].SetCellValue(dto.LevelName);
+                row.Cells[4].SetCellValue(dto.MajorName);
+                row.Cells[5].SetCellValue(dto.StatusName);
+                row.Cells[6].SetCellValue(dto.CreateTimeStr);
+                row.Cells[7].SetCellValue(dto.CreateUserName);
+            }
+
+            //导出
+            this.NPOIExport("报名单列表" + DateTime.Now.ToString("yyyyMMddHHmmss") + ".xls", hssfworkbook, new List<HSSFSheet> { sheet });
+
+            #endregion
+
+            return null;
         }
 
         /// <summary>
--- Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Controllers/BaseController.cs
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Controllers/BaseController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,6 +9,7 @@ using EnrolmentPlatform.Project.DTO.Systems;
 using EnrolmentPlatform.Project.IBLL.Basics;
 using EnrolmentPlatform.Project.IBLL.Orders;
 using EnrolmentPlatform.Project.Infrastructure;
+using NPOI.HSSF.UserModel;
 
 namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers
 {
@@ -122,6 +124,44 @@ namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Controllers
         /// 层级接口
         /// </summary>
         public static IT_SchoolLevelMajorService LevelService= DIContainer.Resolve<IT_SchoolLevelMajorService>();
+
+        #endregion
+
+        #region NPOI模板导出
+
+        /// <summary>
+        /// NPOI组件导出Excel共用方法
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="hssfworkbook"></param>
+        /// <param name="sheets"></param>
+        protected void NPOIExport(string fileName, NPOI.HSSF.UserModel.HSSFWorkbook hssfworkbook, List<HSSFSheet> sheets)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                hssfworkbook.Write(ms);
+                ms.Flush();
+                ms.Position = 0;
+                sheets.ForEach(a =>
+                {
+                    a = null;
+                });
+                hssfworkbook = null;
+
+                byte[] data = ms.ToArray();
+
+                //客户端保存
+                HttpResponse response = System.Web.HttpContext.Current.Response;
+                response.Clear();
+                response.Charset = "UTF-8";
+                Response.ContentEncoding = System.Text.Encoding.UTF8;
+                response.ContentType = "application/vnd-excel";//"application/vnd.ms-excel";
+
+                string encodeFileName = HttpUtility.UrlEncode(fileName, System.Text.Encoding.UTF8).Replace("+", " ");
+                System.Web.HttpContext.Current.Response.AddHeader("Content-Disposition", string.Format("attachment; filename=" + encodeFileName));// Server.UrlEncode(fileName)
+                System.Web.HttpContext.Current.Response.BinaryWrite(data);
+            }
+        }
 
         #endregion
     }
