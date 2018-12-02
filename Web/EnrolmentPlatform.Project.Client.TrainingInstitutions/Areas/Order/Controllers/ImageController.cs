8dd4ce8a78e011a28f0977b92486bb5247f78867
chaichang <chaichang@CHAICHANG-PC>
2018-12-02 18:15:51 +0800

    1

---
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/ImageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Controllers
+{
+    public class ImageController : Controller
+    {
+        // GET: Order/Image
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
