28a0f122951c0324ec4bd8a79859243905ad861d
柴畅 <706659446@qq.com>
2019-10-15 11:04:10 +0800

    1

---
+++ API/EnrolmentPlatform.Project.Domain/Entities/Orders/T_OrderApproval.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrolmentPlatform.Project.Domain.Entities.Orders
+{
+    /// <summary>
+    /// 订单审批表
+    /// </summary>
+    [Serializable]
+    [Table("T_OrderApproval")]
+    [DataContract]
+    public class T_OrderApproval : Entity
+    {
+        /// <summary>
+        /// 订单ID
+        /// </summary>
+        [DataMember]
+        public Guid OrderId { set; get; }
+
+        /// <summary>
+        /// 学生姓名
+        /// </summary>
+        [DataMember]
+        public string StudentName { set; get; }
+
+        /// <summary>
+        /// 身份证号码
+        /// </summary>
+        [DataMember]
+        public string IDCardNo { set; get; }
+
+        /// <summary>
+        /// 手机号码
+        /// </summary>
+        [DataMember]
+        public string Phone { set; get; }
+
+        /// <summary>
+        /// 微信/QQ
+        /// </summary>
+        [DataMember]
+        public string TencentNo { set; get; }
+
+        /// <summary>
+        /// 邮箱
+        /// </summary>
+        [DataMember]
+        public string Email { set; get; }
+
+        /// <summary>
+        /// 性别
+        /// </summary>
+        [DataMember]
+        public string Sex { set; get; }
+
+        /// <summary>
+        /// 民族
+        /// </summary>
+        [DataMember]
+        public string MinZu { set; get; }
+
+        /// <summary>
+        /// 籍贯
+        /// </summary>
+        [DataMember]
+        public string JiGuan { set; get; }
+
+        /// <summary>
+        /// 最高学历
+        /// </summary>
+        [DataMember]
+        public string HighesDegree { set; get; }
+
+        /// <summary>
+        /// 毕业院校
+        /// </summary>
+        [DataMember]
+        public string GraduateSchool { set; get; }
+
+        /// <summary>
+        /// 毕业证编号
+        /// </summary>
+        [DataMember]
+        public string BiYeZhengBianHao { set; get; }
+
+        /// <summary>
+        /// 联系地址
+        /// </summary>
+        [DataMember]
+        public string Address { set; get; }
+
+        /// <summary>
+        /// 工作单位
+        /// </summary>
+        [DataMember]
+        public string GongZuoDanWei { set; get; }
+
+        /// <summary>
+        /// 招生老师
+        /// </summary>
+        [DataMember]
+        public string ZhaoShengLaoShi { set; get; }
+
+        /// <summary>
+        /// 备注
+        /// </summary>
+        [DataMember]
+        public string Remark { set; get; }
+
+        /// <summary>
+        /// 审批状态（0：草稿，1：待审核，2：审核通过，3：审核失败）
+        /// </summary>
+        [DataMember]
+        public int? ApprovalStatus { set; get; }
+
+        /// <summary>
+        /// 审批备注
+        /// </summary>
+        [DataMember]
+        public string ApprovalComment { set; get; }
+    }
+}
---
+++ API/EnrolmentPlatform.Project.Domain/Entities/Orders/T_OrderImageApproval.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrolmentPlatform.Project.Domain.Entities.Orders
+{
+    /// <summary>
+    /// 订单照片
+    /// </summary>
+    [Serializable]
+    [Table("T_OrderImageApproval")]
+    [DataContract]
+    public class T_OrderImageApproval : Entity
+    {
+        /// <summary>
+        /// 订单ID
+        /// </summary>
+        [DataMember]
+        public Guid OrderId { set; get; }
+
+        /// <summary>
+        /// 订单审批ID
+        /// </summary>
+        [DataMember]
+        public Guid OrderApprovalId { set; get; }
+
+        /// <summary>
+        /// 身份证正面
+        /// </summary>
+        [DataMember]
+        public string IDCard1 { set; get; }
+
+        /// <summary>
+        /// 身份证反面
+        /// </summary>
+        [DataMember]
+        public string IDCard2 { set; get; }
+
+        /// <summary>
+        /// 两寸蓝底
+        /// </summary>
+        [DataMember]
+        public string LiangCunLanDiImg { set; get; }
+
+        /// <summary>
+        /// 毕业证
+        /// </summary>
+        [DataMember]
+        public string BiYeZhengImg { set; get; }
+
+        /// <summary>
+        /// 免考英语（社保/居住证正）
+        /// </summary>
+        [DataMember]
+        public string MianKaoYingYuImg { set; get; }
+
+        /// <summary>
+        /// 免考计算机（社保/居住证反）
+        /// </summary>
+        [DataMember]
+        public string MianKaoJiSuanJiImg { set; get; }
+
+        /// <summary>
+        /// 教育部学历证书电子备案表
+        /// </summary>
+        [DataMember]
+        public string XueXinWangImg { set; get; }
+
+        /// <summary>
+        /// 其他
+        /// </summary>
+        [DataMember]
+        public string QiTa { set; get; }
+
+        /// <summary>
+        /// 头像
+        /// </summary>
+        [DataMember]
+        public string TouXiang { set; get; }
+    }
+}
