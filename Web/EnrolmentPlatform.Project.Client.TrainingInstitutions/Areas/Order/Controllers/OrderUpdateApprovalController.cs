a039113bf9f40c55d8e63d20de7759ae98bd3635
柴畅 <706659446@qq.com>
2019-10-16 16:35:11 +0800

    1

--- Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/OrderUpdateApprovalController.cs
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Areas/Order/Controllers/OrderUpdateApprovalController.cs
@@ -46,9 +46,58 @@ namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Cont
         /// <summary>
         /// 订单信息
         /// </summary>
+        /// <param name="orderId">orderId</param>
+        /// <param name="approvalId">approvalId</param>
+        /// <param name="action">action</param>
         /// <returns></returns>
-        public ActionResult OrderInfo()
+        public ActionResult OrderInfo(Guid? orderId,Guid? approvalId,string action)
         {
+            if (orderId.HasValue == false && approvalId.HasValue == false)
+            {
+                return RedirectToAction("Index", "OrderUpdateApproval");
+            }
+
+            OrderDto orderInfo = null;
+            if (orderId.HasValue == true)
+            {
+                var orderApproval = OrderApprovalService.GetOrderApplyApprovalInfoByOrderId(orderId.Value);
+                if (orderApproval != null)
+                {
+                    return OrderInfo(null, orderApproval.ApprovalId, "update");
+                }
+                orderInfo = OrderService.GetOrder(orderId.Value);
+            }
+            else
+            {
+                var orderApproval = OrderApprovalService.GetOrderApplyApprovalInfo(approvalId.Value);
+                if (orderApproval == null)
+                {
+                    return RedirectToAction("Index", "OrderUpdateApproval");
+                }
+                orderInfo = OrderService.GetOrder(orderApproval.OrderId);
+                orderInfo.Address = orderApproval.Address;
+                orderInfo.BiYeZhengBianHao = orderApproval.BiYeZhengBianHao;
+                orderInfo.CreateUserName = orderApproval.ZhaoShengLaoShi;
+                orderInfo.Email = orderApproval.Email;
+                orderInfo.GongZuoDanWei = orderApproval.GongZuoDanWei;
+                orderInfo.GraduateSchool = orderApproval.GraduateSchool;
+                orderInfo.HighesDegree = orderApproval.HighesDegree;
+                orderInfo.IDCardNo = orderApproval.IDCardNo;
+                orderInfo.JiGuan = orderApproval.JiGuan;
+                orderInfo.MinZu = orderApproval.MinZu;
+                orderInfo.Phone = orderApproval.Phone;
+                orderInfo.Remark = orderApproval.Remark;
+                orderInfo.Sex = orderApproval.Sex;
+                orderInfo.StudentName = orderApproval.StudentName;
+                orderInfo.TencentNo = orderApproval.TencentNo;
+            }
+
+            //订单信息
+            ViewBag.OrderInfo = orderInfo;
+            //批次
+            ViewBag.BatchList = MetadataService.GetList(DTO.Enums.Basics.MetadataTypeEnum.Batch);
+            //学校
+            ViewBag.SchoolList = MetadataService.GetList(DTO.Enums.Basics.MetadataTypeEnum.School);
             return View();
         }
 
@@ -117,6 +166,18 @@ namespace EnrolmentPlatform.Project.Client.TrainingInstitutions.Areas.Order.Cont
             {
                 return Json(new { ret = 0, msg = ret.Info });
             }
+        }
+
+        /// <summary>
+        /// 获得层级数据
+        /// </summary>
+        /// <param name="parentId">父类ID</param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult GetLevelData(Guid parentId)
+        {
+            var list = LevelService.FindSubItemById(parentId);
+            return Json(list);
         }
     }
 }
