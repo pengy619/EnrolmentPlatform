2e581c8e238f5e1cbf534ecd30b8f013b7da80b8
柴畅 <706659446@qq.com>
2019-10-31 16:33:17 +0800

    11

--- API/EnrolmentPlatform.Project.IBLL/Basics/IT_CustomerFieldService.cs
+++ API/EnrolmentPlatform.Project.IBLL/Basics/IT_CustomerFieldService.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using EnrolmentPlatform.Project.DTO;
+using EnrolmentPlatform.Project.DTO.Basics;
+using EnrolmentPlatform.Project.Infrastructure;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +14,46 @@ namespace EnrolmentPlatform.Project.IBLL.Basics
     /// </summary>
     public interface IT_CustomerFieldService
     {
+        /// <summary>
+        /// 获得一个学校所有自定义字段列表
+        /// </summary>
+        /// <param name="dto">dto</param>
+        /// <returns></returns>
+        List<CustomerFieldDto> GetAllList(GetAllListSearchDto dto);
 
+        /// <summary>
+        /// 获得一个学校所有自定义字段列表
+        /// </summary>
+        /// <param name="dto">dto</param>
+        /// <returns></returns>
+        GridDataResponse GetList(GetAllListSearchDto dto);
+
+        /// <summary>
+        /// 新增自定义字段
+        /// </summary>
+        /// <param name="dto">dto</param>
+        /// <returns></returns>
+        ResultMsg Add(CustomerFieldDto dto);
+
+        /// <summary>
+        /// 修改自定义字段
+        /// </summary>
+        /// <param name="">dto</param>
+        /// <returns></returns>
+        ResultMsg Update(CustomerFieldDto dto);
+
+        /// <summary>
+        /// 查找自定义字段
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        CustomerFieldDto Find(Guid id);
+
+        /// <summary>
+        /// 删除自定义字段
+        /// </summary>
+        /// <param name="id">id</param>
+        /// <returns></returns>
+        ResultMsg Delete(Guid id);
     }
 }
