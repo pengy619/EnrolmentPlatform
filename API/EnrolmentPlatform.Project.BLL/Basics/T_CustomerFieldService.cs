9184b2a178da59e3098fd9c18aef1be57646dd14
柴畅 <706659446@qq.com>
2019-10-31 15:47:34 +0800

    提交自定义字段！

---
+++ API/EnrolmentPlatform.Project.BLL/Basics/T_CustomerFieldService.cs
@@ -0,0 +1,14 @@
+﻿using EnrolmentPlatform.Project.IBLL.Basics;
+using EnrolmentPlatform.Project.Infrastructure.Castle;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrolmentPlatform.Project.BLL.Basics
+{
+    public class T_CustomerFieldService : IT_CustomerFieldService, IInterceptorLogic
+    {
+    }
+}
---
+++ API/EnrolmentPlatform.Project.IBLL/Basics/IT_CustomerFieldService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EnrolmentPlatform.Project.IBLL.Basics
+{
+    /// <summary>
+    /// 自定义字段服务
+    /// </summary>
+    public interface IT_CustomerFieldService
+    {
+
+    }
+}
