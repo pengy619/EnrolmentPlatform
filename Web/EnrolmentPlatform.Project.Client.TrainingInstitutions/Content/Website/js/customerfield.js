b9ab7e73aabc17255ba5d89549247b89bda9612a
柴畅 <706659446@qq.com>
2019-11-01 17:24:49 +0800

    自定义字段显示完成！

--- Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Content/Website/js/customerfield.js
+++ Web/EnrolmentPlatform.Project.Client.TrainingInstitutions/Content/Website/js/customerfield.js
@@ -6,10 +6,44 @@ function filedList(schoolId, data, beforeContains) {
             contentType: "application/json;utf-8",
             dataType: "json",
             async: false,
-            data: "{schooldId:" + schoolId + "}",
+            data: "{schooldId:'" + schoolId + "'}",
             url: '/Order/Manager/GetCustomerFieldList',
             success: function (result) {
-                alert(JSON.stringify(result));
+                if (result) {
+                    var html = "";
+                    for (var i = 0; i < result.length; i++) {
+                        var cur = result[i];
+                        //段落开头
+                        if (i == 0 || i % 3 == 0) {
+                            html = html + "<div class=\"layui-form-item\">";
+                        }
+
+                        //字段信息
+                        html = html + "<label class=\"layui-form-label\">" + cur.Name + "：</label>";
+                        html = html + "<div class=\"layui-input-inline\">";
+                        if (cur.CustomerFieldType == 1) {
+                            //文本框
+                            html = html + "<input type=\"text\" class=\"layui-input customerField\" name='" + cur.Name + "' maxlength=\"50\" lay-verify=\"required\" value=''>";
+                        }
+                        else if (cur.CustomerFieldType == 2) {
+                            //下拉框
+                            html = html + "<select lay-verify=\"required\" class=\"customerField\" name=\"" + cur.Name + "\">";
+                            html += "<option value=\"\"></option>";
+                            var items = cur.SelectItems.split('|');
+                            for (var j = 0; j < items.length; j++) {
+                                html += "<option value=\"" + items[j] + "\">" + items[j] + "</option>";
+                            }
+                            html = html + "</select>";
+                        }
+                        html = html + "</div>";
+
+                        //段落结尾
+                        if ((i + 1) % 3 == 0) {
+                            html = html + "</div>";
+                        }
+                    }
+                    $("#" + beforeContains).parent().append(html);
+                }
             }
         });
     }
