4c891db8c5487703d6846ff64176bd0f0aa869db
pengy619 <justdoit619@foxmail.com>
2022-05-31 22:01:54 +0800

    11

--- Web/EnrolmentPlatform.Project.Client.Admin/DBScripts/更新脚本.sql
+++ Web/EnrolmentPlatform.Project.Client.Admin/DBScripts/更新脚本.sql
@@ -64,6 +64,6 @@ update T_Permissions set Name='设置招生指标' where Name='设置招生库�
 IF NOT EXISTS (SELECT 1 FROM T_Permissions WHERE Id='8cd58c07-80d1-4c36-9b72-766257b5130a')
 BEGIN
 	INSERT [dbo].[T_Permissions] ([Id], [Name], [Level], [Area], [Controller], [Action], [Param], [Classify], [ParentId], [Sort], [Icon]) VALUES (N'8cd58c07-80d1-4c36-9b72-766257b5130a', N'毕业管理', 3, N'Order', N'Graduation', N'Index', NULL, 3, N'7f3adae1-87a1-4a0f-8ac7-c6120f35832c', 4, N'icon-yunyingguanli')
-	INSERT [dbo].[T_Permissions] ([Id], [Name], [Level], [Area], [Controller], [Action], [Param], [Classify], [ParentId], [Sort], [Icon]) VALUES (N'fc4d1123-2b87-4997-9f01-e49b010e2e50', N'上传毕业照', 4, N'Order', N'Graduation', N'Option', NULL, 3, N'8cd58c07-80d1-4c36-9b72-766257b5130a', 1, NULL)
+	INSERT [dbo].[T_Permissions] ([Id], [Name], [Level], [Area], [Controller], [Action], [Param], [Classify], [ParentId], [Sort], [Icon]) VALUES (N'fc4d1123-2b87-4997-9f01-e49b010e2e50', N'查看毕业照', 4, N'Order', N'Graduation', N'Option', NULL, 3, N'8cd58c07-80d1-4c36-9b72-766257b5130a', 1, NULL)
 END
 GO
